8331543acade1b2ec004d8095f1dae190c861af4
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-22 08:38:02 +0530

    entities code initialized

--- demos/backrooms/main.py
+++ demos/backrooms/main.py
@@ -1,11 +1,19 @@
 import raycaster
 
+def person_handler(person):
+    pass
+
 player = raycaster.Player(0, 0)
+person = raycaster.Entity(10, 10, "assets/person.png", person_handler)
+
 raycaster = raycaster.Raycaster(
     (1200, 700),
-    "demos/backrooms/map.png",
+    "assets/map.png",
     player,
-    background="demos/backrooms/background.png",
+    background="assets/background.png",
+    entities=[person],
 )
+
+
 raycaster.loadMap()
 raycaster.run()
--- raycaster/raycaster.py
+++ raycaster/raycaster.py
@@ -1,4 +1,4 @@
-import pygame, math
+import pygame, math, time
 from PIL import Image
 
 pygame.init()
@@ -9,13 +9,27 @@ class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.angle = 315 * math.pi / 180
-        self.speed = 3
+        self.angle = 0 * math.pi / 180
+        self.speed = 150
 
+class Entity:
+    def __init__(self, x, y, asset, handler):
+        self.x = x
+        self.y = y
+        self.asset = asset
+        self.img = pygame.image.load(asset)
+        self.PIL_img = Image.open(asset)
+        self.handler = handler
+
+    def run(self, screen):
+        self.handler(self)
+        # img_w, img_h = self.img.get_size()
+        # x = self.x - img_w / 2
+        # screen.blit(self.img, (x, self.y))
 
 class Raycaster:
     def __init__(
-        self, size, mapFile, player, fov=90, background="./raycaster/background.png"
+        self, size, mapFile, player, entities=None, fov=90, background="./raycaster/background.png"
     ):
         self.width, self.height = size
         self.screen = pygame.display.set_mode(size)
@@ -26,12 +40,20 @@ class Raycaster:
         self.player = player
         self.font = pygame.font.SysFont("comicsans", 20)
 
+        if not entities:
+            self.entities = []
+        else:
+            self.entities = entities
+
         self.fov = fov * math.pi / 180
         self.max_depth = 640
         self.casted_rays = 240
         self.STEPS = 1
         self.DEBUG = False
+        self.TOPDOWN = False
+        self.DV = (self.width/2) / math.tan(self.fov/2)
 
+        self.mapfile = mapFile
         self.img = Image.open(mapFile)
         self.img = self.img.convert("RGB")
         self.img_height, self.img_width = self.img.size
@@ -75,6 +97,9 @@ class Raycaster:
                 ):
                     break
             depth += 0.0001
+            if self.TOPDOWN:
+                pygame.draw.line(self.screen, (255, 255, 255), (self.player.x, self.player.y),(x, y))
+
 
             start_angle += self.fov / self.casted_rays
             cdep = depth * self.STEPS
@@ -86,31 +111,46 @@ class Raycaster:
 
             depth = cdep * math.cos(self.player.angle - start_angle)
             wall_height = 21000 / (depth)
-            rays.append([wall_height, color_dep, ray, depth])
+            rays.append({"data": [wall_height, color_dep, ray, depth], "type": "wall"})
         # sort rays from shortest to longest
-        rays.sort(key=lambda x: x[0])
+        rays.sort(key=lambda x: x["data"][0])
         return rays
+
+    def load_entities(self):
+        for entity in self.entities:
+            entity.run(self.screen)
+            if self.TOPDOWN:
+                pygame.draw.circle(self.screen, (255, 255, 255), (entity.x, entity.y), 5)
+                pygame.draw.line(self.screen, (255, 255, 0), (entity.x, entity.y), (self.player.x, self.player.y))
+
+    def top_down_view(self):
+        mapfile = pygame.image.load(self.mapfile)
+        self.screen.blit(mapfile, (0, 0))
+        pygame.draw.circle(self.screen, (255, 255, 255), (self.player.x, self.player.y), 5)
+
 
     def load_walls(self, rays):
         SCALE = (self.width) / self.casted_rays
-        for ray in range(self.casted_rays):
-            wall_height, color_dep, num, depth = rays[ray]
-            pygame.draw.rect(
-                self.screen,
-                tuple(color_dep),
-                (
-                    num * SCALE,
-                    (self.height / 2) - wall_height / 2,
-                    SCALE + 1,
-                    wall_height,
-                ),
-            )
+        if not self.TOPDOWN:
+            for ray in range(self.casted_rays):
+                if rays[ray]["type"] == "wall":
+                    wall_height, color_dep, num, depth = rays[ray]["data"]
+                pygame.draw.rect(
+                    self.screen,
+                    tuple(color_dep),
+                    (
+                        num * SCALE,
+                        (self.height / 2) - wall_height / 2,
+                        SCALE + 1,
+                        wall_height,
+                    ),
+                )
 
     def debug_screen(self, rays):
-        player_val = f"Player: x: {self.player.x}, y: {self.player.y}, angle: {self.player.angle}"
+        player_val = f"Player: x: {self.player.x}, y: {self.player.y}, angle: {self.player.angle*180/math.pi%360}"
         fov_val = f"FOV: {self.fov * 180 / math.pi}"
-        furthest_wall = f"Furthest wall: {rays[0][3]}"
-        nearest_wall = f"Nearest wall: {rays[-1][3]}"
+        furthest_wall = f"Furthest wall: {rays[0]['data'][3]}"
+        nearest_wall = f"Nearest wall: {rays[-1]['data'][3]}"
         rays_val = f"Rays: {self.casted_rays}"
         fps = f"FPS: {self.clock.get_fps()}"
         self.screen.blit(self.font.render(player_val, True, (255, 255, 255)), (10, 10))
@@ -125,6 +165,7 @@ class Raycaster:
         self.screen.blit(self.font.render(fps, True, (255, 255, 255)), (10, 110))
 
     def run(self):
+        prev_time = time.time()
         while self.running:
             pxData = self.pxData
 
@@ -135,13 +176,15 @@ class Raycaster:
             self.screen.fill((0, 0, 0))
             self.screen.blit(self.background, (0, 0))
 
-            delta_time = self.clock.tick(120) / 10 + 1
+            now = time.time()
+            delta_time = now - prev_time
+            prev_time = now
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_a]:
-                self.player.angle -= 0.05 * delta_time
+                self.player.angle -= 2 * delta_time
             if keys[pygame.K_d]:
-                self.player.angle += 0.05 * delta_time
+                self.player.angle += 2 * delta_time
             if keys[pygame.K_w]:
                 nextX = (
                     self.player.x
@@ -173,9 +216,14 @@ class Raycaster:
                 ] == (0, 0, 0):
                     self.player.x = nextX
                     self.player.y = nextY
-
+            if keys[pygame.K_ESCAPE]:
+                self.TOPDOWN = not self.TOPDOWN
+            
+            if self.TOPDOWN:
+                self.top_down_view()
             rays = self.cast_rays()
             self.load_walls(rays)
+            self.load_entities()
             if keys[pygame.K_x]:
                 self.DEBUG = not self.DEBUG
 
