7ed5598e36de14fd6f5e1f458db4ed25fca870fd
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-10-23 10:24:11 +0530

    player movement

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 from raycaster import Raycaster
 import pygame
+import math
+
+class Player:
+    def __init__(self, pos, angle, speed):
+        self.pos = pos
+        self.angle = angle
+        self.speed = speed
+
+    def move(self, dist):
+        self.pos[0] += math.sin(math.radians(self.angle)) * dist
+        self.pos[1] += math.cos(math.radians(self.angle)) * dist
 
 class Game:
     def __init__(self):
-        self.raycaster = Raycaster((640, 320), (0, 0), fov=90)
+        self.player = Player([10, 10], 0, 0.1)
+        self.raycaster = Raycaster((640, 320), self.player, fov=90)
         self.raycaster.load_map("./assets/map.png")
         self.running = True
+        
 
     def run(self):
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                self.player.move(self.player.speed)
+            if keys[pygame.K_s]:
+                self.player.move(-self.player.speed)
+            if keys[pygame.K_a]:
+                self.player.angle += 0.5
+            if keys[pygame.K_d]:
+                self.player.angle -= 0.5
 
             self.raycaster.update()
+            pygame.display.update()
 
 if __name__ == "__main__":
     game = Game()
--- raycaster.py
+++ raycaster.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 from PIL import Image
 
 class Raycaster:
@@ -7,13 +7,19 @@ class Raycaster:
         self.background = pygame.image.load(background)
         self.background = pygame.transform.scale(self.background, size)
         self.map = []
+        self.map_img = None
+        self.player = player
 
     def load_map(self, map_file):
         img = Image.open(map_file)
         img = img.convert("RGBA")
         self.map = list(img.getdata())
-        print(self.map)
+        self.map_img = pygame.image.load(map_file)
 
     def update(self):
         self.screen.blit(self.background, (0, 0))
-        pygame.display.update()
+        if self.map_img:
+            self.screen.blit(self.map_img, (0, 0))
+        pygame.draw.circle(self.screen, (255, 0, 0), self.player.pos, 5)
+        # draw a line facing the player
+        pygame.draw.line(self.screen, (255, 0, 0), self.player.pos, (self.player.pos[0] + 100 * math.sin(math.radians(self.player.angle)), self.player.pos[1] + 100 * math.cos(math.radians(self.player.angle))))
