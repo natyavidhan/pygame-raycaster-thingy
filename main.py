3387f1bf1744651ce69a619e1c0b29c70d0c6a51
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-12-16 11:04:56 +0530

    another casual re write

--- main.py
+++ main.py
@@ -2,24 +2,14 @@ from raycaster import Raycaster
 import pygame
 import math
 
-class Player:
-    def __init__(self, pos, angle, speed):
-        self.pos = pos
-        self.angle = angle
-        self.speed = speed
-
-    def move(self, dist):
-        dx = dist * math.sin(math.radians(self.angle))
-        dy = dist * math.cos(math.radians(self.angle))
-        return (self.pos[0] + dx, self.pos[1] + dy)
-
 class Game:
     def __init__(self):
-        self.player = Player([10, 10], 0, 1)
-        self.raycaster = Raycaster((640, 320), self.player, fov=90)
-        self.raycaster.load_map("./assets/maze.png")
+        self.screen = pygame.display.set_mode((800, 600))
         self.running = True
-        
+        self.raycaster = Raycaster(self.screen)
+
+        pygame.display.set_caption("Raycaster")
+        self.clock = pygame.time.Clock()
 
     def run(self):
         while self.running:
@@ -27,27 +17,14 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            pygame.display.set_caption("FPS: " + str(round(self.raycaster.clock.get_fps())))
+            self.clock.tick(60)
+            self.screen.fill((0, 0, 0))
 
-            delta_time = self.raycaster.clock.tick(600) / 10
+            self.raycaster.cast_rays()
 
-            new_pos = (self.player.pos[0], self.player.pos[1])
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_w]:
-                new_pos = self.player.move(self.player.speed * delta_time)
-            if keys[pygame.K_s]:
-                new_pos = self.player.move(-self.player.speed * delta_time)
-            if keys[pygame.K_a]:
-                self.player.angle -= 0.75 * delta_time
-            if keys[pygame.K_d]:
-                self.player.angle += 0.75 * delta_time
-            
-            if self.raycaster.valid_pos(new_pos):
-                self.player.pos = new_pos
-
-            self.raycaster.update()
-            pygame.display.update()
+            pygame.display.flip()
 
 if __name__ == "__main__":
+    pygame.init()
     game = Game()
     game.run()
--- raycaster.py
+++ raycaster.py
@@ -1,65 +1,8 @@
-import pygame, math
-from PIL import Image
+import pygame
 
 class Raycaster:
-    def __init__(self, size, player, entities=None, fov=90, background="./assets/back.png"):
-        self.screen = pygame.display.set_mode(size)
-        self.background = pygame.image.load(background)
-        self.background = pygame.transform.scale(self.background, size)
-        self.map = []
-        self.map_img = None
-        self.player = player
-        self.clock = pygame.time.Clock()
-        self.fov = fov
-        self.rays = 240
-        
-    def load_map(self, map_file):
-        img = Image.open(map_file)
-        img = img.convert("RGBA")
-        self.map = list(img.getdata())
-        self.map_img = pygame.image.load(map_file)
-    
-    def valid_pos(self, pos):
-        return self.map[int(pos[1]) * self.map_img.get_width() + int(pos[0])] == (0, 0, 0, 0)
+    def __init__(self, screen):
+        self.screen = screen
 
     def cast_rays(self):
-        angle = self.player.angle - self.fov / 2
-        rays = []
-        for i in range(self.rays):
-            angle += self.fov / self.rays
-            l = self.cast_ray(angle)
-            rays.append([l, i, angle])
-        self.draw_walls(rays)
-
-
-    def cast_ray(self, angle):
-        l = 0
-        while True:
-            l += 0.25
-            x = self.player.pos[0] + l * math.sin(math.radians(angle))
-            y = self.player.pos[1] + l * math.cos(math.radians(angle))
-            if not self.valid_pos((x, y)) or l > 1000:
-                return l
-
-    def draw_walls(self, rays: list[float], draw_rays = False):
-        rays.sort(key=lambda x: x[0])
-        SCALE = self.screen.get_width() / self.rays
-        for i, ray in enumerate(rays):
-            if draw_rays:
-                pygame.draw.line(self.screen, (255, 255, 0), self.player.pos, (self.player.pos[0] + ray[0] * math.sin(math.radians(ray[1])), self.player.pos[1] + ray[0] * math.cos(math.radians(ray[1]))))            
-            depth = max(ray[0], 0.0001)
-            color = 255 / (1 + depth * depth * 0.0001)
-            depth*=math.cos(math.radians(ray[2] - self.player.angle))
-            h = 15000 / depth
-            s_height = self.screen.get_height()
-            s_width = self.screen.get_width()
-            pygame.draw.rect(self.screen, (color, color, color), (ray[1] * SCALE, s_height / 2 - h / 2, SCALE+0.5, h))
-
-    def map_value(self, var, min, max, new_min, new_max):
-        return (var - min) / (max - min) * (new_max - new_min) + new_min
-
-    def update(self):
-        self.screen.blit(self.background, (0, 0))
-        self.cast_rays()
-        self.screen.blit(pygame.transform.scale(self.map_img, (150, 150)), (0, 0))
-        pygame.draw.circle(self.screen, (255, 0, 0), (int(self.map_value(self.player.pos[0], 0, self.map_img.get_width(), 0, 150)), int(self.map_value(self.player.pos[1], 0, self.map_img.get_height(), 0, 150))), 5)
+        pass
